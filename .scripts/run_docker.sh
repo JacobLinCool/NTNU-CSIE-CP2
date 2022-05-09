020a03ce3642df3ea4aa7439b2f14db0292b5eb6
JacobLinCool <jacoblincool@gmail.com>
2022-05-09 20:43:36 +0800

    chore: simplify run docker script

--- .scripts/run_docker.sh
+++ .scripts/run_docker.sh
@@ -1,11 +1,2 @@
 # Run Container
-docker run -dit -v $PWD/source:/usr/workspace -w /usr/workspace --name cp-homework-container cp-homework
-
-# Enter Interactive Shell
-docker exec -it cp-homework-container bash
-
-# Stop Container
-docker stop cp-homework-container
-
-# Remove Container
-docker rm cp-homework-container
+docker run -it --rm -v $PWD/source:/usr/workspace -w /usr/workspace cp-homework bash
